b847d4963a80adf0d877f496995eae06cdd722cf
zard01 <areesha.durrani@hotmail.com>
2022-10-05 18:43:14 +0100

    Finished solution

--- primes.py
+++ primes.py
@@ -2,5 +2,20 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+  list = [2]
+  numToTest=2
+  while len(list)!=number_of_primes:
+    numToTest+=1
+
+    isPrime=True
+
+    if numToTest%2:
+      for i in range (3,(numToTest+1)//2,2):
+        if numToTest%i==0: 
+          isPrime=False
+          break
+      
+      if isPrime:
+        list.append(numToTest)
+
+  return list
